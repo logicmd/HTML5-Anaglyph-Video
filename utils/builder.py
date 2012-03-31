afa637270525406f861bd346ae24fd0fb3968a78
Kevin Tong <logicmd@gmail.com>
2012-03-31 12:23:38 +0800

    WIP

--- utils/builder.py
+++ utils/builder.py
@@ -1,47 +1,40 @@
 ## build video anaglyph js
 import os
+from obfuscate import obfuscate
 
+def buildit(sourcePath, buildPath, licensePath):
+    os.system( 'java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + sourcePath + ' --js_output_file ' + buildPath )
+
+    buildFile = open( buildPath, 'r' )
+    contents = buildFile.read();
+    buildFile.close()
+
+    licenseFile = open( licensePath, 'r' )
+    license_contents = licenseFile.read();
+    licenseFile.close()
+
+    buildFile = open( buildPath, 'w' )
+    buildFile.write( license_contents + contents )
+    buildFile.close()
+
+## here comes
+obfuscate()
+
+#build anaglyph js
 source = 'middle.js'
 build = '../build/video.anaglyph.js'
 license = '../src/LICENSE'
 
-os.system( 'java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + source + ' --js_output_file ' + build )
-
-buildFile = open( build, 'r' )
-contents = buildFile.read();
-buildFile.close()
-
-licenseFile = open( license, 'r' )
-license_contents = licenseFile.read();
-licenseFile.close()
-
-buildFile = open( build, 'w' )
-buildFile.write( license_contents + contents )
-buildFile.close()
+buildit(source, build, license)
 
 ## build video js
-source = 'middle2.js'
+source2 = 'middle2.js'
 build = '../build/video.patched.js'
 license = '../src/LICENSE-VideoJS'
 
-os.system( 'java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + source + ' --js_output_file ' + build )
-
-buildFile = open( build, 'r' )
-contents = buildFile.read();
-buildFile.close()
-
-licenseFile = open( license, 'r' )
-license_contents = licenseFile.read();
-licenseFile.close()
-
-buildFile = open( build, 'w' )
-buildFile.write( license_contents + contents )
-buildFile.close()
-
-## more obfuscate
-buildFile = open( build, 'r' )
-contents = buildFile.read();
-buildFile.close()
+buildit(source2, build, license)
 
 
+os.remove(source)
+os.remove(source2)
 
